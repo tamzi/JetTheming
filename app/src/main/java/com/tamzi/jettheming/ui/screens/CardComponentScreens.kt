4d1155667f7a5f50cf5cd2eb7baa7179e8ad9cab
tamzi <tamrefrank@gmail.com>
2021-10-18 23:58:28 -1000

    created text composables

--- app/src/main/java/com/tamzi/jettheming/ui/screens/CardComponentScreens.kt
+++ app/src/main/java/com/tamzi/jettheming/ui/screens/CardComponentScreens.kt
@@ -1,7 +1,19 @@
 package com.tamzi.jettheming.ui.screens
 
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
 class CardComponentScreens {
 
 
 
+}
+
+@Composable
+fun HomeScreen(/*...*/) {
+    Scaffold(
+        drawerContent = { /*...*/ },
+        topBar = { /*...*/ },
+        content = { /*...*/ }
+    )
 }
